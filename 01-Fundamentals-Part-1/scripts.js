0f7d4ba85cbdf0bb654f561f7ab4cabdc68ac747
Pumkincode <sarajuliaerdei@gmail.com>
2024-12-17 14:15:17 +0100

    first commit

---
+++ 01-Fundamentals-Part-1/scripts.js
@@ -0,0 +1,44 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+//standar variable name in JavaScript(camelCase notation)
+let firstName = "Leno";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.145
+
+let myFirstJob = "Programmer"
+
+console.log(myFirstJob)
+
+true;
+let JavascriptIsFun = true;//boolean
+console.log(JavascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+//dynamic typing
+JavascriptIsFun = 'YES!';//string
+console.log(typeof JavascriptIsFun);
+
+//undefined/empty value
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
